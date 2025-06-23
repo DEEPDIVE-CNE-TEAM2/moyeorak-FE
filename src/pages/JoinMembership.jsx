113ab2a3be2c9384abcd96d33ade2c289cc3e650
Qoopkite <qotodus@inha.edu>
2025-06-23 17:46:52 +0900

    [FEAT] Card component 작성

--- src/pages/JoinMembership.jsx
+++ src/pages/JoinMembership.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Card from '../component/card/Card'
+import img from '../img/testimg.jpg'
 
 const JoinMembership = () => {
   return (
+    <>
     <div>JoinMembership</div>
+        <Card
+      imageUrl={img}
+      title="제목"
+      details={[
+        '성인 남녀',
+        '2025.07.01 ~ 2025.07.15',
+        '2025.08.01 ~ 2025.08.31',
+        '30,000원'
+      ]}
+    />
+    </>
   )
 }
 
